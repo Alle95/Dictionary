0cf0061116852cd4b02809d99b0151c0e36b34ca
Alle95 <147329984+Alle95@users.noreply.github.com>
2023-10-26 12:46:24 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let word;
+let string = "";
+
+function dictionary() {
+    if(document.getElementById('newWord') != null) {
+        word = document.getElementById('newWord').value;
+        console.log(word);
+        string = string + word + " ";
+        console.log(string);
+    }
+}
+
+function searchWord() {
+    if(document.getElementById('existWord') != null) {
+        word = document.getElementById('existWord').value;
+        if(string.indexOf(word) !== -1) {
+            alert("This word can be found in the dictionary!")
+        } else {
+            alert("This word is not found in the dictionary!")
+        }
+    }
+}
+
+function resetInput() {
+    let getValue= document.getElementById("newWord");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }   
+    let getValue2 = document.getElementById("existWord");
+    if (getValue2.value !="") {
+        getValue2.value = "";
+    } 
+}
